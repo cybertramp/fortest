0f5537733e723b6104f792a6660cecf3a4521158
cybertramp <paran_son@outlook.com>
2018-10-21 18:48:51 +0900

    Update pipe3.c

--- pipe/pipe3.c
+++ pipe/pipe3.c
@@ -20,14 +20,14 @@ int main(void){
 		dup(pfd[1]);	// 표준 출력을 파이프 입력으로 연결
 		close(pfd[0]);
 
-		execlp("ls","ls",NULL);		// 표준 출력으로 나와야 하지만 파이프의 입력으로 들어감
+		execlp("ls","ls",NULL);		// 표준 출력으로 나와야 하지만 파이프의 입력으로 들어감(파일 실행함수 : execlp)
 	}
 	else{
 		close(0);	// 표준 입력 닫기
 		dup(pfd[0]);	// 표준 입력을 파이프의 출력으로 연결
 		close(pfd[1]);
 
-		execlp("wc","wc","-l",NULL);	// 파이프의 출력(자식의 ls 한 결과)이 wc의 입력이 
+		execlp("wc","wc","-l",NULL);	// 파이프의 출력(자식의 ls 한 결과)이 wc의 입력으로(파일 실행함수 : execlp)
 	}
 
 	return 0;
