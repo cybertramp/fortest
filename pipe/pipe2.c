b4993f29a4db42168609d926947a24470e689103
cybertramp <paran_son@outlook.com>
2018-10-21 18:41:41 +0900

    Update pipe2.c

--- pipe/pipe2.c
+++ pipe/pipe2.c
@@ -5,29 +5,35 @@
 #include<sys/wait.h>
 #include<unistd.h>
 
+/*
+fork-----pfd[1]----->wait(null)		 // 부모
+ㄴ-------pfd[0]-->			// 자식
+*/
 int main(void){
 	int pfd[2];
 	char buf[30];
 
 	/* create pipe */
-	if(pipe(pfd) == -1){
+	if(pipe(pfd) == -1){	// -1인 경우 pipe 에러
 		perror("pipe error");
 		exit(1);
 	}
-	if(!fork()){/* Create child */
+	/* Create child */
+	if(!fork()){		// pid에 해당하는 자식이 생성됨
 		printf("CHILD(PID: %d): Writing to the pipe\n",getpid());
 
-		write(pfd[1], "pipe!",5);
+		write(pfd[1], "pipe!",5);	// pfd[1]에 "pipe!" 전달
 
 		exit(0);
 	}
-	else{/* for parent */
+	/* for parent */
+	else{			// 부모
 		printf("PARENT(PID: %d): Reading from the pipe\n",getpid());
 
-		read(pfd[0],buf,5);
+		read(pfd[0],buf,5);	// pfd[0] 값을 받아 buf에 저장
 		printf("PARENT(PID : %d): %s\n",getuid(),buf);
 
-		wait(NULL);
+		wait(NULL);	// 기다림
 	}
 	return 0;
 }
